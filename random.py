22d4451f384cb9cdcf3318f0d0f65f7a1f48e6d6
Shubhashree Barik <102846296+ShubhaBarik@users.noreply.github.com>
2022-05-09 16:32:03 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 17:17:20 2022
+
+@author: shree_codes
+"""
+
+# of randint() function
+# imports random module
+
+#For 40 particles total
+int count = 0;
+count +=1;
+
+#for loop
+
+for (int i = 1; i<=40; i++):
+   {
+import random
+{
+# Generates a random number between
+#0 and 20 
+r1 = random.randit(0, 20)
+print("Random number in range is % s" % (r1))
+
+r2 = random.randit(0, 20)
+print("Random number in range is % d" % (r2))
+
+r3 = random.randit(0, 20)
+print("Random number in range is % d" % (r3))
+}
+break
+   }
